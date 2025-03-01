7378dac9057601fbeda58a2d292816ad5b559213
Vikrant Chauhan <vikrantchauhan1047@gmail.com>
2025-03-01 14:39:11 +0530

    .

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -1,5 +1,4 @@
-import {createClient} from "@supabase/supabase-js";
-
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -7,10 +6,7 @@ const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY) {
     throw new Error("Missing Supabase environment variables!");
 }
-const supabase =createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-    
-);
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default supabase;
